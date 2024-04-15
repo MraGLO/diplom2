2796545c8a1b039afd52a7eeb8994bd199d17a3b
Admin_Mark <mrag.forgames@gmail.com>
2024-04-15 14:17:01 +0300

    builder.Services.AddDbContext<DiplomDbContext>();

--- diplom/Program.cs
+++ diplom/Program.cs
@@ -1,10 +1,11 @@
-
+using diplom.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DiplomDbContext>();
 
 var app = builder.Build();
 
